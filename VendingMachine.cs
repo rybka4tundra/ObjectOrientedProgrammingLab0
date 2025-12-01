0c389c37924a03b901fad3575f0a087d74896375
rybka <RybkinAndreyMB@yandex.ru>
2025-12-02 01:05:04 +0300

    Write main functionality for VendingMachine class

--- VendingMachine.cs
+++ VendingMachine.cs
@@ -1,4 +1,25 @@
 class VendingMachine
 {
-    private List<Product> Products {get; set;} = new List<Product>(); 
+    private List<int> AcceptableNominalsValues {get; set;}
+    private List<CoinStack> Deposit {get; set;}
+    private List<ProductStack> Products {get; set;}
+
+    public VendingMachine() {
+        AcceptableNominalsValues = new List<int> {1,2,5,10};
+        Deposit = AcceptableNominalsValues.Select(value => new CoinStack(new Coin(value), 0)).ToList();
+        Products = new List<ProductStack>();
+    }
+
+    public int DepositValue{
+        get => Deposit.Sum(CoinStack => CoinStack.Value);
+    }
+
+    public bool InsertCoin(int CoinValue){
+        if (!AcceptableNominalsValues.Contains(CoinValue)){
+            return false;
+        } else {
+            Deposit.Find(CoinStack => CoinStack.Nominal.Value == CoinValue).Count += 1;
+            return true;
+        }
+    } 
 }
