217a20bbfaf6eee323ed2ad839c3c6683cb4b6da
rybka <RybkinAndreyMB@yandex.ru>
2025-12-01 16:38:13 +0300

    Add basic logic to program such ass CLI

--- Program.cs
+++ Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    private static VendingMachine VendingMachine {get; set;} = new VendingMachine();
+    private static bool IsWorkingFlag = true;
+
+    //Greates user
+    private static void Great() {
+        Console.WriteLine("--Vending Machine by Rybkin Andrey--");
+    }
+
+    //Sugests the user to enter a command and return this command
+    private static string Command() {
+        Console.WriteLine("Please enter command:");
+        var command = Console.ReadLine(); 
+        if (command != null) {
+            return command;
+        } else {
+            return "";
+        }
+        
+    }
+
+    //Prints list of commands
+    private static void Help() {
+        Console.WriteLine(@"
+        help - Prints list of commands
+        list - Lists all available products and their price and count
+        deposit - Insert coins in vending machine
+        select - Select product and get it if there is enough coins on deposit. Decrease deposit by price of product
+        change - Get left deposit
+        admin - Enter admin mode
+        quit - Quits program");
+    }
+    
+    //Quits programm
+    private static void Quit() {
+        IsWorkingFlag = false;
+    }
+
+    static void Main(string[] args)
+    {    
+        Great();
+        while (IsWorkingFlag)
+        {
+            Help();
+            switch (Command()) {
+                case "help":
+                    Help();
+                    break;
+                case "quit":
+                    Quit();
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
