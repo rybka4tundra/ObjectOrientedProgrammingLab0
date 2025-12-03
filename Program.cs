b18c657d387503688516e5d6f68811d060945463
rybka <RybkinAndreyMB@yandex.ru>
2025-12-03 17:20:21 +0300

    Replace switch case with PromptExecutor function for more customizability and readability of Program, Deposit and other future mods. Select inserting a coin into Vending machine to a another function.

--- Program.cs
+++ Program.cs
@@ -7,11 +7,42 @@
     //Reads users prompt and return command from it 
     private static string Prompt() {
         var line = Console.ReadLine();
-        if (String.IsNullOrWhiteSpace(line)) {
+        if (string.IsNullOrWhiteSpace(line)) {
             return "";
         } else {
             return line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[0];
         }
+    }
+
+    //Executes a non numeric prompt e.g. "help", "list"
+    private static bool NonNumericPromptExecutor(string prompt, Dictionary<string, Action> commandMap) {
+        bool res = commandMap.TryGetValue(prompt, out Action action);
+        if (res) {
+            action();
+        }
+        return res;
+    }
+
+    //Executes a numeric prompt e.g. "123", "0111"
+    private static bool NumericPromptExecutor(string prompt, Action<int> numericAction) {
+        bool res = int.TryParse(prompt, out int ignoreInt);
+        if (res) {
+            numericAction(ignoreInt);
+        }
+        return res;
+    }
+
+    //Executes single prompt
+    private static bool PromptExecutor(Dictionary<string, Action> commandMap, Action<int> numericAction = null) {
+        string prompt = Prompt();
+        if (NonNumericPromptExecutor(prompt, commandMap)) {
+            return true;
+        } else if (numericAction != null && NumericPromptExecutor(prompt, numericAction)) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     //Prints rules for command entering
@@ -46,31 +77,25 @@
         DepositModeIsWorkingFlag = false;
     }
 
-    //Insert coins in vending machine
+    //Inserts coin into vending machine
+    private static void InsertCoin(int coinValue) {
+        if (VendingMachine.InsertCoin(coinValue)) {
+            Console.WriteLine("SUCCESS: Coin was put in deposit");
+        } else {
+            Console.WriteLine("FAIL: Coins with this nominal is unacaptable");
+        }
+    }
+
+    //Enter deposit mode
     private static void DepositMode() {
         DepositModeIsWorkingFlag = true;
         DepositModeHelp();
+        Dictionary<string, Action> commandMap = new Dictionary<string, Action>();
+        commandMap.Add("help", DepositModeHelp);
+        commandMap.Add("quit", QuitDepositMode);
         while (DepositModeIsWorkingFlag)
         {
-            string prompt = Prompt();
-            int ignoreInt;
-            if (int.TryParse(prompt, out ignoreInt)) {
-                bool isCoinInserted = VendingMachine.InsertCoin(int.Parse(prompt));
-                if (!isCoinInserted){
-                    Console.WriteLine("Coins with this nominal is unacaptable");
-                }
-            } else {
-                switch (prompt) {
-                    case "help":
-                        DepositModeHelp();
-                        break;
-                    case "quit":
-                        QuitDepositMode();
-                        break;
-                    default:
-                        break;
-                }
-            } 
+            PromptExecutor(commandMap, InsertCoin); 
         }
     }
 
@@ -95,28 +120,17 @@
         ProgramModeIsWorkingFlag = false;
     }
 
-    //Insert coins in vending machine
+    //Enter program mode
     private static void ProgramMode() {
         ProgramModeHelp();
+        Dictionary<string, Action> commandMap = new Dictionary<string, Action>();
+        commandMap.Add("help", ProgramModeHelp);
+        commandMap.Add("list", List);
+        commandMap.Add("deposit", DepositMode);
+        commandMap.Add("quit", QuitProgramMode);
         while (ProgramModeIsWorkingFlag)
         {
-            switch (Prompt()) {
-                case "help":
-                    ProgramModeHelp();
-                    break;
-                case "list":
-                    List();
-                    break;
-                case "deposit":
-                    DepositMode();
-                    break;
-                case "quit":
-                    QuitProgramMode();
-                    break;
-                default:
-                    break;
-
-            }
+            PromptExecutor(commandMap); 
         }
     }
 
