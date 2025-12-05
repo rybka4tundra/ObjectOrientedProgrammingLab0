4a9a8f4d3c93bd1425bbe27ae9d8ffd87ffe2d16
rybka <RybkinAndreyMB@yandex.ru>
2025-12-05 18:48:44 +0300

    Change a bit Prompt, so now it skips whitespaces or nulls until a valid prompt. Remove NonNumericPromptExecutor and NumericPromptExecutor. Change them to tryExecuteMainCommand delegate. Change help text for deposit mode. Add Admin and Add modes.

--- Program.cs
+++ Program.cs
@@ -3,46 +3,32 @@
     private static VendingMachine VendingMachine {get; set;} = new VendingMachine();
     private static bool ProgramModeIsWorkingFlag = true;
     private static bool DepositModeIsWorkingFlag = false;
+    private static bool AdminModeIsWorkingFlag = false;
+    private static bool AddModeIsWorkingFlag = false;
 
-    //Reads users prompt and return command from it 
+    //Reads users prompt and return a command from it 
     private static string Prompt() {
-        var line = Console.ReadLine();
-        if (string.IsNullOrWhiteSpace(line)) {
-            return "";
-        } else {
-            return line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[0];
+        string? line;
+        while(true){
+            line = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(line)) {
+                break;
+            }
         }
-    }
-
-    //Executes a non numeric prompt e.g. "help", "list"
-    private static bool NonNumericPromptExecutor(string prompt, Dictionary<string, Action> commandMap) {
-        bool res = commandMap.TryGetValue(prompt, out Action action);
-        if (res) {
-            action();
-        }
-        return res;
-    }
-
-    //Executes a numeric prompt e.g. "123", "0111"
-    private static bool NumericPromptExecutor(string prompt, Action<int> numericAction) {
-        bool res = int.TryParse(prompt, out int ignoreInt);
-        if (res) {
-            numericAction(ignoreInt);
-        }
-        return res;
+        return line.Split(" ", StringSplitOptions.RemoveEmptyEntries)[0];
     }
 
     //Executes single prompt
-    private static bool PromptExecutor(Dictionary<string, Action> commandMap, Action<int> numericAction = null) {
+    private static bool PromptExecutor(Dictionary<string, Action> commandMap, Func<string, bool>? tryExecuteMainCommand = null) {
         string prompt = Prompt();
-        if (NonNumericPromptExecutor(prompt, commandMap)) {
+        if (commandMap.TryGetValue(prompt, out Action action)) {
+            action();
             return true;
-        } else if (numericAction != null && NumericPromptExecutor(prompt, numericAction)) {
+        } else if (tryExecuteMainCommand != null && tryExecuteMainCommand(prompt)) {
             return true;
         } else {
             return false;
         }
-
     }
 
     //Prints rules for command entering
@@ -66,7 +52,7 @@
         Console.WriteLine("""
         --Deposit mode help--
         help - Prints list of commands for deposit mode.
-        <int> - Insert coin with nominal <int>.
+        <int CoinNominal> - Insert coin with nominal <int>.
         quit - Quit putting a deposit.
 
         """);
@@ -86,6 +72,16 @@
         }
     }
 
+    //Check if prompt can be parsed and if yes than executes main command 
+    private static bool TryExecuteDeposit(string prompt){
+        if (int.TryParse(prompt, out int parsedPrompt)) {
+            InsertCoin(parsedPrompt);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     //Enter deposit mode
     private static void DepositMode() {
         DepositModeIsWorkingFlag = true;
@@ -95,7 +91,91 @@
         commandMap.Add("quit", QuitDepositMode);
         while (DepositModeIsWorkingFlag)
         {
-            PromptExecutor(commandMap, InsertCoin); 
+            PromptExecutor(commandMap, TryExecuteDeposit); 
+        }
+    }
+
+    //Prints list of commands for add mode
+    private static void AddModeHelp() {
+        CommandRules();
+        Console.WriteLine("""
+        --Add mode help--
+        help - Prints list of commands for add mode.
+        <string ProductName>:<int ProductPrice>:<int ProductCount> - adds a stack of product with name, price and count.
+        quit - Quit putting a deposit.
+
+        """);
+    }
+
+    //Check if prompt can be parsed and if yes than executes main command 
+    private static bool TryExecuteAdd(string prompt){
+        string[] tokens = prompt.Split(':');
+        if (tokens.Length != 3){
+            return false;
+        }else if (int.TryParse(tokens[1], out int parsedPrice) && int.TryParse(tokens[2], out int parsedCount)) {
+            VendingMachine.AddProductStack(tokens[0], parsedPrice, parsedCount);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Quits add mode
+    private static void QuitAddMode() {
+        AddModeIsWorkingFlag = false;
+    }
+
+    //Enter add mode
+    private static void AddMode() {
+        AddModeIsWorkingFlag = true;
+        AddModeHelp();
+        Dictionary<string, Action> commandMap = new Dictionary<string, Action>();
+        commandMap.Add("help", AddModeHelp);
+        commandMap.Add("quit", QuitAddMode);
+        while (AddModeIsWorkingFlag)
+        {
+            PromptExecutor(commandMap,TryExecuteAdd); 
+        }
+    }
+
+    //Prints list of commands for admin mode
+    private static void AdminModeHelp() {
+        CommandRules();
+        Console.WriteLine("""
+        --Admin mode help--
+        help - Prints list of commands for admin mode.
+        list - Lists all available products and their price and count.
+        collect - collects income.
+        add - Enter add mode.
+        delete - Enter delete mode.
+        quit - Quit admin mode.
+
+        """);
+    }
+
+    //Collects money from vending machine
+    private static void Collect() {
+        Console.WriteLine($"From vending machine were collected: {VendingMachine.Collect()}");
+    }
+
+    //Quits admin mode
+    private static void QuitAdminMode() {
+        AdminModeIsWorkingFlag = false;
+    }
+
+    //Enter admin mode
+    private static void AdminMode() {
+        AdminModeIsWorkingFlag = true;
+        AdminModeHelp();
+        Dictionary<string, Action> commandMap = new Dictionary<string, Action>();
+        commandMap.Add("help", AdminModeHelp);
+        commandMap.Add("list", List);
+        commandMap.Add("collect", Collect);
+        commandMap.Add("add", AddMode);
+        commandMap.Add("quit", QuitAdminMode);
+        while (AdminModeIsWorkingFlag)
+        {
+            PromptExecutor(commandMap); 
         }
     }
 
@@ -127,6 +207,7 @@
         commandMap.Add("help", ProgramModeHelp);
         commandMap.Add("list", List);
         commandMap.Add("deposit", DepositMode);
+        commandMap.Add("admin", AdminMode);
         commandMap.Add("quit", QuitProgramMode);
         while (ProgramModeIsWorkingFlag)
         {
